5be712a7305412e5ae4082ecb3ddc7b121de4a36
Robertson <b_robertson@tss-hr420-dm07.fc.ca>
2018-02-09 11:24:03 -0500

    updates

--- app.js
+++ app.js
@@ -1,36 +1,32 @@
-const express = require('express');
+const express = require('express');//include this, just like a php onclude
 const app = express();
 const io = require('socket.io')();
 
 // serve up static files
 app.use(express.static('public'));
 
-//add modules
+// add routes
 app.use(require('./routes/index'));
 app.use(require('./routes/contact'));
 app.use(require('./routes/portfolio'));
 
-//app.get('/contact', (req, res)=>{
-//  res.sendFile(__dirname + "/contact.html");
-//});
-
-//app.get('/portfolio', (req, res)=>{
-//  res.sendFile(__dirname + "/portfolio.html");
-//});
 
 const server = app.listen(3000, () => {
-  console.log('listening on port 3000');
+  console.log('listening on the port 3000');
 });
 
 io.attach(server);
 
 io.on('connection', socket => { //function(socket) {...}
-    console.log('a user has connected and its working fine');
-    io.emit('chat message', { for : 'everyone', message : `${socket.id} is here`});
+  console.log('a user has connected!');
+  io.emit('chat message', { for : 'everyone', message : `${socket.id} is here!` });
 
-    socket.on('disconnect', () => {
-      console.log('a user disconnected');
+socket.on('chat message', msg =>{
+  io.emit('chat message', {for : 'everyone', message : msg});
+});
 
-      io.emit('disconnect message', `${socket.id} has left tht ebuilding`);
-    });
+  socket.on('disconnect', () => {
+    console.log("a user disconnected");
+    io.emit('disconnect message', `${socket.id} has left the building!`);
+  });
 });
--- public/js/main.js
+++ public/js/main.js
@@ -1,17 +1,27 @@
 (() => {
   const socket = io();
 
-  let messageList = document.querSelector('ul'),
+  let messageList = document.querySelector('ul'),
   chatForm = document.querySelector('form'),
-  chatMessage = chatForm.querySelector('.message');
+  nameInput = document.querySelector('.nickname'),
+  chatMessage = chatForm.querySelector('.message'),
+  nickName = null;
+
+  function setNickname() {
+    nickName = this.value;
+  }
 
   function handleSendMessage(e) {
     e.preventDefault();
-    debugger;
-  }
+   nickName = (nickName && nickName.length > 0) ? nickName : 'user';
+   msg = `${nickName} says ${chatMessage.value}`;
+
+   socket.emit('chat message', msg);
+   checkMessage.value = "";
+   return false;
+}
 
   function appendMessage(msg) {
-  //  debugger;
   let newMsg = `<li>${msg.message}</li>`;
   messageList.innerHTML += newMsg;
   }
@@ -22,9 +32,8 @@
   }
 
 
+  nameInput.addEventListener('chamge', setNickname, false);
   chatForm.addEventListener('submit', handleSendMessage, false);
   socket.addEventListener('chat message', appendMessage, false);
   socket.addEventListener('disconnect message', appendDiscMessage, false);
-
-
 })();
